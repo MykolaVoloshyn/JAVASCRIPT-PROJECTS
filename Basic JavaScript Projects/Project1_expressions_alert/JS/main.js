171d422c5077a1951435d83dcfd89a495f202dff
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-03-06 16:49:38 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello, World!"); //Causes an alert window to pop up in the browser when enter the web page
+
+var sent1, sent2; //Defining variables
+
+sent1 = "This is the beggining of the string "; //Giving this variable a string value
+sent2 = "and this is the end of the string."; //Giving this variable a string value
+
+document.write(sent1 + sent2 + "<br>"); //Concatenates string values of two variables and adds a new line
+
+document.write("7 + 1 =", 7 + 1); //Calculates an expression and write it in the page
