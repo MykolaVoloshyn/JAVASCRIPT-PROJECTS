de2a013894d75f10ae973d1fc22dd61b2d755868
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-03-07 21:51:50 -0800

    PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,17 @@
+document.write(typeof 5.2); //Displays the data type of a variable
+document.write("<br>");
+document.write(8 + "7"); //Displays the result of type coercion when adding together number and string data types
+document.write("<br>");
+document.write(82 == "82"); //Displays the result of checking two arguments for equality
+document.write("<br>");
+document.write(82 === "82"); //Displays the result of checking two arguments for equality and data type
+document.write("<br>");
+document.write(10 > 9); //Displays the result of checking if the first argument is bigger then the second
+document.write("<br>");
+document.write(7 < 5); //Displays the result of checking if the first argument is smaller then the second
+document.write("<br>");
+document.write(33 == "33" && typeof 1 == "number"); //Utilizing the AND operator to display the result of checking two groups of arguments for equality
+document.write("<br>");
+document.write(33 === "33" || typeof 1 == "number"); //Utilizing the OR operator to display the result of checking two groups of arguments for equality and data type
+document.write("<br>");
+document.write(!(5 > 10)); //Utilizing the NOT operator to display the opposite result of checking if the first argument is bigger then the second
